0c7361d35d548cf3cd39a9ef8652373c1adfe26f
xyang4 <18610450436@163.com>
2019-09-19 12:03:53 +0800

    添加logback相关案例

---
+++ logback/src/main/java/com/example/logback/IOrderService.java
@@ -0,0 +1,12 @@
+package com.example.logback;
+
+/**
+ * @author xYang
+ * @date 2019/9/19 0019 10:50
+ * @purchase //TODO 一句话说明
+ */
+
+public interface IOrderService {
+
+    void printPrice(String goodsId, int num);
+}
